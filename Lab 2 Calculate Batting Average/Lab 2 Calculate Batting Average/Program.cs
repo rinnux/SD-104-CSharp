2bd2746ef7effcca0c50ecfa2452115d9fc381b3
Warin Yoongram <rinnux@yahoo.com>
2019-08-05 13:12:05 -0500

    commit

---
+++ Lab 2 Calculate Batting Average/Lab 2 Calculate Batting Average/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Project:    Lab 2 Calculate Batting Average
+Date:        to/da/y
+Author:     You the big new programmer
+Notes:      This program reads in a baseball player's name,
+            number of hits and number of at bats.
+            The player's batting average is calculated and displayed
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Calculate_Batting_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //varibalbe declaration
+            string strPlayerName;
+            int nHits, nAtBats;
+            double dblBattingAverage;
+            //prompt for name an receive
+            Console.WriteLine("This program calclates a baseball player's batting average");
+            Console.WriteLine("<--------------------------------===>");
+            Console.WriteLine("Enter the player's name: ");
+            strPlayerName = Console.ReadLine();
+            //promt for hits
+            Console.WriteLine("Eenter the player's # of hits");
+            nHits = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the palyer's # of at bats: ");
+            nAtBats = Convert.ToInt32(Console.ReadLine());
+            //Calculation
+            dblBattingAverage = nHits / nAtBats;
+            Console.WriteLine(strPlayerName + "'s batting average is " + dblBattingAverage);
+            Console.ReadLine();
+
+
+                
+        }
+    }
+}
