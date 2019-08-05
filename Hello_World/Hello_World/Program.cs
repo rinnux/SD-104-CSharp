67e5dd7d516983ffce263f24bf3bfe29d5084872
Warin Yoongram <rinnux@yahoo.com>
2019-08-05 12:24:13 -0500

    Com

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -10,6 +10,9 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
