e552c5e409046b58f5840122fdf2cf73f74f8162
Warin Yoongram <rinnux@yahoo.com>
2019-08-05 14:21:34 -0500

    commit

---
+++ Lab 3 BMI Calculator/Lab 3 BMI Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_BMI_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            double dblHeight;
+            double dblWeight;
+            double dblBMI;
+
+            //user input
+            Console.WriteLine("Enter your Height in inches: ");
+            dblHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your Weight in pounds: ");
+            dblWeight = Convert.ToInt32(Console.ReadLine());
+
+            //calculations
+            dblBMI = (dblWeight * 703) / (dblHeight * dblHeight);
+
+
+            //issue result and message
+            Console.WriteLine("Your BMI is " + dblBMI);
+            if(dblBMI < 18.5)
+            {
+                Console.WriteLine("BMI < 18.5 -Underweight");
+            }
+            else if(dblBMI <= 24.9)
+            {
+               Console.WriteLine("BMI < 24.9 -Normal");
+
+            }
+            else if(dblBMI <= 29.9)
+            {
+                Console.WriteLine("BMI < 18.5 -Overweight");
+
+            }
+            else
+            {
+                Console.WriteLine("BMI >=30 -Obese");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
