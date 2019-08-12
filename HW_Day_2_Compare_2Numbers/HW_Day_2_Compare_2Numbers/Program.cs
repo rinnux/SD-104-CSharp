b2a5f4823e4d7fa4956461542080d7dc4c0c2bfb
Warin Yoongram <rinnux@yahoo.com>
2019-08-12 09:06:24 -0500

    Com

---
+++ HW_Day_2_Compare_2Numbers/HW_Day_2_Compare_2Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Day_2_Compare_2Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstNumber, secondNumber;
+            Console.WriteLine("This Program compare two number and display the large number");
+            Console.WriteLine("<----------------------------------------------------------->");
+            Console.WriteLine("Enter first number :");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("First number is larger than Second number");
+            }
+            else if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("Both numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Second Number is larger");
+            }
+   
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework Day 2_Conditional Practice/Homework Day 2_Conditional Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Day_2_Conditional_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
