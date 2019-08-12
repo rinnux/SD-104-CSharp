bc6b794c5628a15084a0232f8a630246d3513e57
Warin Yoongram <rinnux@yahoo.com>
2019-08-12 10:33:42 -0500

    commit

--- HW_Day_2_Compare_3Numbers/HW_Day_2_Compare_3Numbers/Program.cs
+++ HW_Day_2_Compare_3Numbers/HW_Day_2_Compare_3Numbers/Program.cs
@@ -10,17 +10,26 @@ namespace HW_Day_2_Compare_3Numbers
     {
         static void Main(string[] args)
         {
-
-            int firstNumber, secondNumber, thirdNumber, maxNumber, minNumber;
+            //Declare varialbles
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+            int maxNumber;
+            int minNumber;
+            // promt user to enter three numbers
             Console.WriteLine("This program will compare three number");
             Console.WriteLine("<-------------------------------------->");
+            //promt for the first number
             Console.WriteLine("Enter first number :");
             firstNumber = Convert.ToInt32(Console.ReadLine());
+            //promt for the second number
             Console.WriteLine("Enter second number :");
             secondNumber = Convert.ToInt32(Console.ReadLine());
+            //promt for the third number
             Console.WriteLine("Enter third number :");
             thirdNumber = Convert.ToInt32(Console.ReadLine());
 
+            // compare three numbers to find max number
             if (firstNumber > secondNumber && firstNumber > thirdNumber)
             {
                 maxNumber = firstNumber;
@@ -35,6 +44,7 @@ namespace HW_Day_2_Compare_3Numbers
                 maxNumber = thirdNumber;
             }
 
+            // compare three numbers to find min number
             if (firstNumber < secondNumber && firstNumber < thirdNumber)
             {
                 minNumber = firstNumber;
@@ -49,7 +59,7 @@ namespace HW_Day_2_Compare_3Numbers
                 minNumber = thirdNumber;
             }
 
-
+            //print the results
             Console.WriteLine(" First Number : " + firstNumber + " Second Number : " + secondNumber + " thirdNumber : " + thirdNumber + " MaxNumber is : " + maxNumber);
             Console.WriteLine(" First Number : " + firstNumber + " Second Number : " + secondNumber + " thirdNumber : " + thirdNumber + " MinNumber is : " + minNumber);
 
