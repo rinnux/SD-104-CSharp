d88020ddd6097636d16574458ba8023a3a2b08ef
Warin Yoongram <rinnux@yahoo.com>
2019-08-15 12:52:38 -0500

    Com

---
+++ CheckEmployee/CheckEmployee/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee employee1 = new employee();
+            employee1.name = " Warin";
+            employee1.lastName = "Yoongram";
+            employee1.department = null;
+
+            currentDepartment(employee1);
+            Console.WriteLine(employee1.department);
+            Console.ReadLine();
+        }
+
+        static void currentDepartment(employee isEmployed)
+        {
+            if(isEmployed == null)
+            {
+                Console.WriteLine("He is in IT Department");
+              
+            }
+            else
+            {
+                Console.WriteLine("He is in HR Department");
+            }
+        }
+    }
+    
+   
+    class employee
+    {
+        public string name;
+        public string lastName;
+        public string department;
+    }
+}
---
+++ ConsoleAppEmployee/ConsoleAppEmployee/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee myEmployee = new employee();
+            myEmployee.FirstName = "John";
+            myEmployee.LastName = "Doe";
+            myEmployee.Dept = null;
+
+            AssignDept(myEmployee);
+            Console.WriteLine(myEmployee.Dept);
+            Console.ReadLine();
+        }
+
+
+        static void AssignDept (employee emp)
+        {
+            if (string.IsNullOrEmpty(emp.Dept))
+            {
+                emp.Dept = "Sale";
+            }
+        }
+
+        class employee
+        {
+            public string FirstName;
+            public string LastName;
+            public string Dept;
+        }
+    }
+}
