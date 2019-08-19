8142bd9f42a5a3d5952492ff770f67102c333344
Warin Yoongram <rinnux@yahoo.com>
2019-08-19 10:49:22 -0500

    Commit

---
+++ ConsoleAppDay6/ConsoleAppDay6/employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay6
+{
+    class employee
+    {
+        string firstName;
+        string lastName;
+        string dept;
+
+        public employee(string firstName, string lastName, string dept)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dept = dept;
+
+            if (string.IsNullOrEmpty(dept))
+            {
+                dept = "IT";
+            }
+
+            Console.WriteLine("First Name : " + firstName + " Last Name " + lastName + " Dept " + dept);
+            Console.ReadLine();
+        }
+    }
+}
