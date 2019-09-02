cd5f53f06b3657c93321adc9cb1133ea87248fbd
Warin Yoongram <rinnux@yahoo.com>
2019-09-02 11:12:33 -0500

    com

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World, Welcometo the Store!";
+        }
+        public IActionResult WhiteRAbbit()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult CancelOrders()
+        {
+            return View();
+        }
+    }
+}
