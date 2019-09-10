b0d9988595be1d189c5633f9d2ea87cadfeedbf6
warin yoongram <rinnux@yahoo.com>
2019-09-09 20:46:35 -0500

    Commit

---
+++ WebApplicationMusicStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationMusicStore.Models;
+
+namespace WebApplicationMusicStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<WebApplicationMusicStore.Models.Song> Songs { get; set; }
+    }
+}
